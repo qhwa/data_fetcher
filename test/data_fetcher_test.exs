a8b557d8acb6630aeec438e5f651da406781fd67
qhwa <qhwa@163.com>
2021-05-24 21:03:19 +0800

    add more testing

--- test/data_fetcher_test.exs
+++ test/data_fetcher_test.exs
@@ -14,4 +14,32 @@ defmodule DataFetcherTest do
       assert DataFetcher.result(:foo) == BAR
     end
   end
+
+  describe "fetch periodicly" do
+    setup do
+      {:ok, pid} = Agent.start_link(fn -> 0 end)
+      [agent: pid]
+    end
+
+    test "it works", %{agent: agent} do
+      opts = [
+        name: :advance,
+        fetcher: fn ->
+          Agent.get_and_update(agent, fn x ->
+            {
+              {:ok, x},
+              x + 1
+            }
+          end)
+        end,
+        interval: 100,
+        cache_storage: DataFetcher.CacheStorage.PersistentTerm
+      ]
+
+      {:ok, _} = DataFetcher.Supervisor.start_link(opts)
+
+      :timer.sleep(1050)
+      assert DataFetcher.result(:advance) == 10
+    end
+  end
 end
