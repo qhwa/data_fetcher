ada21e9c2c438407a8360ad2379d15e7bd7fb2c3
qhwa <qhwa@163.com>
2021-05-25 09:53:33 +0800

    improve performance by avoid messaging

--- lib/data_fetcher/cache.ex
+++ lib/data_fetcher/cache.ex
@@ -1,7 +1,7 @@
 defmodule DataFetcher.Cache do
   @moduledoc false
 
-  use Agent
+  use Task
 
   @default_storage Application.compile_env(
                      :data_fetcher,
@@ -12,17 +12,28 @@ defmodule DataFetcher.Cache do
   def start_link(opts) do
     storage = Keyword.get(opts, :cache_storage, @default_storage)
 
-    Agent.start_link(
-      fn ->
-        storage.init(opts)
-        storage
-      end,
-      name: via_tuple(opts[:name])
-    )
+    define_storage_module(opts[:name], storage)
+
+    Task.start_link(fn ->
+      storage.init(opts)
+    end)
   end
 
-  defp via_tuple(name),
-    do: {:via, Registry, {DataFetcher.Registry.registry_name(name), :cache}}
+  defp define_storage_module(name, storage) do
+    module_definition(name, storage)
+    |> Code.eval_quoted()
+  end
+
+  defp module_definition(name, storage) do
+    quote do
+      defmodule unquote(storage_mod(name)) do
+        def adapter, do: unquote(storage)
+      end
+    end
+  end
+
+  defp storage_mod(name),
+    do: Module.concat([__MODULE__, StorageDef, name])
 
   def put(name, result),
     do: storage_adapter(name).put(name, result)
@@ -31,5 +42,5 @@ defmodule DataFetcher.Cache do
     do: storage_adapter(name).get(name)
 
   defp storage_adapter(name),
-    do: Agent.get(via_tuple(name), & &1)
+    do: storage_mod(name) |> apply(:adapter, [])
 end
